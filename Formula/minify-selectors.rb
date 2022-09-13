7cadbf719e171dd9ba4c0e4205b451ae9f0fada4
Adam Gian <contact@adamgian.com>
2022-09-13 22:39:21 +1000

    chore: v1.0.0-beta.3 release

--- Formula/minify-selectors.rb
+++ Formula/minify-selectors.rb
@@ -1,9 +1,9 @@
 class MinifySelectors < Formula
 	desc "Post-processor to minify class and ID selector names"
 	homepage "https://github.com/adamgian/minify-selectors"
-	url "https://github.com/adamgian/minify-selectors/archive/refs/tags/v0.11.3.tar.gz"
-	sha256 "7310954cc1b1ccf601db1e4cbe996c8dc0bfb221c2e58e7444f6ceb82e944d73"
-	version "0.11.3"
+	url "https://github.com/adamgian/minify-selectors/archive/refs/tags/v1.0.0-beta.3.tar.gz"
+	sha256 "5f585caee76a98ed1b79ad1eac903448cfb7902a53df853887176a4452adcbcb"
+	version "1.0.0-beta.3"
 	depends_on "rust" => :build
 
 	def install
